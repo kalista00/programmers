66e8df7f44298b27b2e68be33e629f92cf930d4e
김창희 <122184547+kalista00@users.noreply.github.com>
2023-07-17 21:46:09 +0900

    Create 쿼드압축 후 개수세기.java

---
+++ 쿼드압축 후 개수세기.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[][] arr) {
+        int[] answer = {};
+        //호출(length, arr)
+        
+        //리턴 받으면 0과 1 개수 세서 배열에 넣고 답 제출
+        return answer;
+    }
+    
+    private int[] divide(int size, int[][] arr){
+    // 나누기 size/2
+    // 나눈 크기로 search 호출(탐색)
+    // 본인호출    
+    }
+    
+    private int[][] search(int size, int[][] arr){
+    //arr 탐색 (size 크기에 해당되는 값 List에 넣기)
+    //List 를 check로 보내기 -> boolean 받기
+        //true 면 0번째 index만 남겨두고 다 -1로 바꾸고 return
+        //false 면 그대로 return
+    }
+
+    private boolean check(List list){
+    //같은지 확인   
+    }
+}
+//arr.length
+//
+// 0 0
+// 0 0
+// dto 만들기
+    
+// 비교할 구간 재귀호출 -> size/2
+    // 탈출정의
+    // 재반복 정의
+        //
+
+////////
+
+//크기정의 (반복)
+	//해당된 크기로 탐색 (반복)
+		//같은지 확인
+		//같을 때 -> 재귀
+		//다를 때 -> 객체 전달
+//리턴 받으면 0과 1 개수 세서 배열에 넣고 답 제출
