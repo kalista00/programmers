1286a0231483248dec04b9d47dadaa0068c5e61a
김창희 <122184547+kalista00@users.noreply.github.com>
2023-08-10 22:04:27 +0900

    Create 피보나치 수.java

---
+++ 피보나치 수.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    public long solution(int n) {
+        long answer = 0;
+        ArrayList<Long> list = new ArrayList<>();
+        list.add(0L);
+        list.add(1L);
+        for(int i = 2; i <= n; i++){
+            list.add((list.get(i-1) + list.get(i-2))  % 1234567);
+        }
+        answer = list.get(n);
+        return answer;
+    }
+
+}
