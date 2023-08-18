eb3f41e21dca32031127e6c46050ca73f96acd98
김창희 <122184547+kalista00@users.noreply.github.com>
2023-08-19 00:14:48 +0900

    Update 크레인 인형뽑기 게임.java

--- 크레인 인형뽑기 게임.java
+++ 크레인 인형뽑기 게임.java
@@ -4,13 +4,29 @@ class Solution {
     private static Stack<Integer> stack;
     private static int disappeared = 0;
     
-    private void method(int move){
+    private void method(int move , int i){
+        if(i == board.length){
+            return;
+        }else if(board[i][move-1] != 0){
+            checkStack(board[i][move-1]);
+            return;
+        }
+        method(move, ++i);
         
+    }
+    private void checkStack(int element){
+        if(!stack.empty() && stack.peek() == element){
+            stack.pop();
+            disappeared += 2;
+        }else{
+            stack.push(element);
+        }
     }
     public int solution(int[][] board, int[] moves) {
         this.board = board;
+        this.stack = new Stack<Integer>();
         for(int move : moves){
-            method(move);
+            method(move, 0);
         }
         int answer = disappeared;
         return answer;
