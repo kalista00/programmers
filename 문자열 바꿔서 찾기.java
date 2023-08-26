6b2bd14b1a2a22c78ac0175659543265b43db14d
김창희 <122184547+kalista00@users.noreply.github.com>
2023-08-26 19:35:06 +0900

    Update 문자열 바꿔서 찾기.java

--- 문자열 바꿔서 찾기.java
+++ 문자열 바꿔서 찾기.java
@@ -1,1 +1,16 @@
-
+class Solution {
+    //StringBuilder builder
+    public int solution(String myString, String pat) {
+        int answer = 0;
+        String abc = "";
+        char[] array = myString.toCharArray();
+        for(int i = 0; i < array.length; i++){
+            array[i] = array[i] == 'A' ? 'B' : 'A';
+            abc = abc + String.valueOf(array[i]);
+        }
+        if(abc.contains(pat)){
+            answer = 1;
+        }
+        return answer;
+    }
+}
