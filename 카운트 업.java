95b870475af43030f5422c728970103eac345e4f
김창희 <122184547+kalista00@users.noreply.github.com>
2023-08-26 10:44:36 +0900

    Create 카운트 업.java

---
+++ 카운트 업.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public int[] solution(int start_num, int end_num) {
+        List<Integer> list = new ArrayList<>();
+        for(int i = start_num ; i <= end_num; i++){
+            list.add(i);
+        }
+        int[] answer = list.stream()
+            .mapToInt(Integer::intValue)
+            .toArray();
+        return answer;
+    }
+}
