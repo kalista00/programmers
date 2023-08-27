dc9790d52c5ec4b5ff037fb1313bbddab95e4cce
김창희 <122184547+kalista00@users.noreply.github.com>
2023-08-27 10:21:10 +0900

    Create n 번째 원소까지.java

---
+++ n 번째 원소까지.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] num_list, int n) {
+        
+        List<Integer> list = new ArrayList<>();
+        for(int i = 0 ; i < n; i++){
+            list.add(num_list[i]);
+        }
+        int[] answer = list.stream()
+            .mapToInt(Integer::intValue)
+            .toArray();
+        return answer;
+    }
+}
