782b6db849dfaebcaa86b83523e204b138528b85
김창희 <122184547+kalista00@users.noreply.github.com>
2023-08-27 10:40:23 +0900

    Create 접두사인지 확인하기.java

---
+++ 접두사인지 확인하기.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Solution {
+    public int solution(String my_string, String is_prefix) {
+        int answer = 0;
+        List<String> list = new ArrayList<>();
+        for(int i = 0; i < my_string.length();i++){
+            list.add(my_string.substring(0,i + 1));
+        }
+        if(list.contains(is_prefix)){
+            answer = 1;
+        }
+        return answer;
+    }
+}
