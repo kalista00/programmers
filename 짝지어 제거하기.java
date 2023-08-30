f6b745bb95a4696d3a4d62da497747b6c25eda8f
김창희 <122184547+kalista00@users.noreply.github.com>
2023-08-30 21:27:49 +0900

    Update 짝지어 제거하기.java

--- 짝지어 제거하기.java
+++ 짝지어 제거하기.java
@@ -1,30 +1,14 @@
-//미완성
-import java.util.*;
+import java.util.Stack;
 
 class Solution{
-    private int answer = 0;
-    private StringBuilder builder = new StringBuilder("");
     
-    private void method(String s){
-        if(s.equals("")){
-            return;
-        }
-        List<String> list = new ArrayList<>();
-        for(int i = 0; i < s.length() - 1; i++){
-            if(!s.substring(i, i + 1).equals(s.substring(i + 1, i + 2))){
-                list.add(s.substring(i, i + 2));
-            }else{
-                s.substring(i, i + 2) = "";
-            }
-        }
-        return;
-    }
     public int solution(String s){
+        Stack<Character> stack = new Stack<>();
+        char[] toChar = s.toCharArray();
         
+        int answer = -1;
+    
 
         return answer;
     }
 }
-//boolean 타입으로 확인하는 매소드
-//if()문으로 위의 탈출조건적용
-//
