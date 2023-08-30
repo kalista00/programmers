a784762190c0c3d41abb0a82fa8662f3ad01ff7e
김창희 <122184547+kalista00@users.noreply.github.com>
2023-08-30 22:05:48 +0900

    Update 짝지어 제거하기.java

--- 짝지어 제거하기.java
+++ 짝지어 제거하기.java
@@ -1,14 +1,22 @@
-import java.util.Stack;
+import java.util.*;
+import java.util.stream.*;
 
 class Solution{
+    private int answer = 0;
+    
+    private void method(Stack<Character> stack){
+        
+    }
     
     public int solution(String s){
         Stack<Character> stack = new Stack<>();
         char[] toChar = s.toCharArray();
-        
-        int answer = -1;
-    
+        IntStream.range(0, toChar.length)
+            .mapToObj(i -> toChar[i])
+            .forEach(stack::push);
+        method(stack);
 
+        
         return answer;
     }
 }
