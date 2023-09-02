8db5d02ee9d60b49c8ac07d886adb4efa919eaed
김창희 <122184547+kalista00@users.noreply.github.com>
2023-09-02 21:12:44 +0900

    Update 나누어 떨어지는 숫자 배열.java

--- 나누어 떨어지는 숫자 배열.java
+++ 나누어 떨어지는 숫자 배열.java
@@ -1,11 +1,15 @@
 import java.util.Arrays;
 class Solution {
     public int[] solution(int[] arr, int divisor) {
+        int[] non1= {-1};
         int[] answer = Arrays.stream(arr)
             .sorted()
             .filter(n -> n % divisor == 0)
             .toArray();
-        if
+        if(answer.length == 0){
+            System.out.println("d");
+            return non1;
+        }
         return answer;
     }
 }
