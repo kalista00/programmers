5fabd08b555fe8ed650fae7fef808c2dd48bf803
김창희 <122184547+kalista00@users.noreply.github.com>
2023-09-22 00:03:14 +0900

    Update 문자열 내림차순으로 배치하기.java

--- 문자열 내림차순으로 배치하기.java
+++ 문자열 내림차순으로 배치하기.java
@@ -1,6 +1,14 @@
+import java.util.Arrays;
+import java.util.Collections;
+
 class Solution {
     public String solution(String s) {
         String answer = "";
+        char[] array = s.toCharArray();
+        Arrays.sort(array);
+        for(int i = array.length -1 ; i >= 0; i--){
+            answer += array[i];
+        }
         return answer;
     }
 }
