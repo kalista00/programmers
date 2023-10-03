1cec4f2163e5fad6a42e1317c493979ce9403349
김창희 <122184547+kalista00@users.noreply.github.com>
2023-10-03 19:19:10 +0900

    Update [1차]비밀지도.java

--- [1차]비밀지도.java
+++ [1차]비밀지도.java
@@ -1,23 +1,41 @@
+import java.util.*;
 
 class Solution {
     int[][] map;
-    private void method(char[] location, int num){
-        for(int i = this.map.length; i >= this.map.length - location.length; i--){
-            //this.map[num][i-1] = Integer.valueOf(location.substring(location.length()-i-1,location.length()-i));
+    private void method(char[] location, int i){
+        int num = this.map.length;
+        int loca = location.length;
+        for(int a = num - 1 ; a >=  num - location.length; a--){
+            --loca;
+            if(location[loca] - 48 == 1){
+                map[i][a] = 1;
+            }
         }
     }
+    
     public String[] solution(int n, int[] arr1, int[] arr2) {
         this.map = new int[n][n];
-        String[] answer = {};
+        
         //StringInteger.valueOf()
         for(int i = 0; i < arr1.length;i++){
             char[] b = Integer.toString(arr1[i], 2).toCharArray();
+            char[] c = Integer.toString(arr2[i], 2).toCharArray();     
             method(b ,i);
-            //a.substring(a.length-i-1,a.length()-i);
+            method(c ,i);
         }
-        
-        //while()
-        //System.out.println(map[n-1][n-1]);
+        String[] answer = new String[arr1.length];
+        for(int j = 0; j < this.map.length; j++){
+            String answ = "";
+            for(int z = 0; z < this.map[0].length; z++){
+                if(map[j][z] == 1){
+                    answ += "#";
+                }
+                else{
+                    answ += " ";
+                }
+            }
+            answer[j] = answ;
+        }
         return answer;
     }
 }
